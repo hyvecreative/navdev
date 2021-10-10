1a82e1fbe6396f5468134999346f39f283b41e4a
Andrew <andrew@hyve.com.au>
2021-10-10 11:19:46 +1100

    css cross on nav (not svg)

--- wp-content/themes/navdev/footer.php
+++ wp-content/themes/navdev/footer.php
@@ -77,6 +77,20 @@
 
 <?= wp_footer() ?> 
 
+<script>
+	const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});// JavaScript Document
+</script>
+
 </body>
 
 </html>
